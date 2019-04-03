1688ee9dfe95a3ffe4589395ec0aa55591d09ec2
tarunb12 <tbelani@ufl.edu>
2019-04-03 03:55:56 -0400

    polynomial comparison

--- poly.ml
+++ poly.ml
@@ -38,13 +38,10 @@ let rec from_expr (e : Expr.expr) : polyExpr =
         else [Term (0, 0)] in
       Times (pow_exp i [])) ;;
 
-(* 
-  Compute degree of a polynomial expression.
-
-  Hint 1: Degree of Term(n,m) is m
-  Hint 2: Degree of Plus[...] is the max of the degree of args
-  Hint 3: Degree of Times[...] is the sum of the degree of args 
-*)
+(*  Compute degree of a polynomial expression.
+    Hint 1: Degree of Term(n,m) is m
+    Hint 2: Degree of Plus[...] is the max of the degree of args
+    Hint 3: Degree of Times[...] is the sum of the degree of args *)
 let rec degree (e : polyExpr) : int =
   match e with
   | Term (_, p) -> p
@@ -66,46 +63,41 @@ let rec degree (e : polyExpr) : int =
     | hd :: tl -> if (degree hd) > (degree (Divide tl)) then degree hd else degree (Divide tl))
   | Negate poly_expr -> degree poly_expr ;;
 
-(* 
-  Comparison function useful for sorting of Plus[..] args 
-  to "normalize them". This way, terms that need to be reduced
-  show up one after another.
-  *)
+(*  Comparison function useful for sorting of Plus[..] args 
+    to "normalize them". This way, terms that need to be reduced
+    show up one after another *)
 let compare (e1 : polyExpr) (e2 : polyExpr) : bool =
   degree e1 > degree e2 ;;
 
-(* Print a polyExprr nicely 
-  Term(3,0) -> 3
-  Term(5,1) -> 5x 
-  Term(4,2) -> 4x^2
-  Plus... -> () + () 
-  Times ... -> ()() .. ()
+(*  Print a polyExprr nicely 
+    Term(3,0) -> 3
+    Term(5,1) -> 5x 
+    Term(4,2) -> 4x^2
+    Plus... -> () + () 
+    Times ... -> ()() .. ()
 
-  Hint 1: Print () around elements that are not Term() 
-  Hint 2: Recurse on the elements of Plus[..] or Times[..]
-*)
+    Hint 1: Print () around elements that are not Term() 
+    Hint 2: Recurse on the elements of Plus[..] or Times[..] *)
 let print_polyExpr (_e : polyExpr): unit =
   (* TODO *)
   Printf.printf "Not implemented";
   print_newline ()
 
-(* 
-  Function to simplify (one pass) polyExprr
+(*  Function to simplify (one pass) polyExprr
 
-  n1 x^m1 * n2 x^m2 -> n1*n2 x^(m1+m2)
-  Term(n1,m1)*Term(n2,m2) -> Term(n1*n2,m1+m2)
+    n1 x^m1 * n2 x^m2 -> n1*n2 x^(m1+m2)
+    Term(n1,m1)*Term(n2,m2) -> Term(n1*n2,m1+m2)
 
-  Hint 1: Keep terms in Plus[...] sorted
-  Hint 2: flatten plus, i.e. Plus[ Plus[..], ..] => Plus[..]
-  Hint 3: flatten times, i.e. times of times is times
-  Hint 4: Accumulate terms. Term(n1,m)+Term(n2,m) => Term(n1+n2,m)
-          Term(n1, m1)*Term(n2,m2) => Term(n1*n2, m1+m2)
-  Hint 5: Use distributivity, i.e. Times[Plus[..],] => Plus[Times[..],]
-    i.e. Times[Plus[Term(1,1); Term(2,2)]; Term(3,3)] 
-      => Plus[Times[Term(1,1); Term(3,3)]; Times[Term(2,2); Term(3,3)]]
-      => Plus[Term(2,3); Term(6,5)]
-  Hint 6: Find other situations that can arise
-*)
+    Hint 1: Keep terms in Plus[...] sorted
+    Hint 2: flatten plus, i.e. Plus[ Plus[..], ..] => Plus[..]
+    Hint 3: flatten times, i.e. times of times is times
+    Hint 4: Accumulate terms. Term(n1,m)+Term(n2,m) => Term(n1+n2,m)
+            Term(n1, m1)*Term(n2,m2) => Term(n1*n2, m1+m2)
+    Hint 5: Use distributivity, i.e. Times[Plus[..],] => Plus[Times[..],]
+      i.e. Times[Plus[Term(1,1); Term(2,2)]; Term(3,3)] 
+        => Plus[Times[Term(1,1); Term(3,3)]; Times[Term(2,2); Term(3,3)]]
+        => Plus[Term(2,3); Term(6,5)]
+    Hint 6: Find other situations that can arise *)
 let simplify_polyExpr (e : polyExpr) : polyExpr =
   match e with
   | Term(_, _) -> Term(0, 1)
@@ -114,8 +106,8 @@ let simplify_polyExpr (e : polyExpr) : polyExpr =
 
 (* Compute if two polyExpr are the same 
    Make sure this code works before you work on simplify1 *)
-let compare_poly_list (poly_expr_list_1 : polyExpr list) (poly_expr_list_2 : polyExpr list) : bool =
-  if poly_expr_list_1 = poly_expr_list_2 then true
+let compare_poly_list (list1 : polyExpr list) (list2 : polyExpr list) : bool =
+  if list1 = list2 then true
   else false ;;
 
 let rec equal_polyExpr (e1 : polyExpr) (e2 : polyExpr) : bool =
@@ -132,10 +124,8 @@ let rec equal_polyExpr (e1 : polyExpr) (e2 : polyExpr) : bool =
   | (Negate expr1, Negate expr2) -> equal_polyExpr expr1 expr2
   | _ -> false ;;
 
-(* Fixed point version of simplify1 
-  i.e. Apply simplify_polyExpr until no 
-  progress is made
-*)    
+(*  Fixed point version of simplify1 
+    i.e. Apply simplify_polyExpr until no progress is made *)    
 let rec simplify (e : polyExpr) : polyExpr =
   let rE = simplify_polyExpr e in
     print_polyExpr rE;
